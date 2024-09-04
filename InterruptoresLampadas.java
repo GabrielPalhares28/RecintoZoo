f77ed6d09cf7c996542897f5ee9d2a9df3881aed
Gabriel Alves Palhares <113656051+GabrielPalhares28@users.noreply.github.com>
2024-09-04 18:27:33 -0300

    Add files via upload

---
+++ InterruptoresLampadas.java
@@ -0,0 +1,13 @@
+public class InterruptoresLampadas {
+    public static void main(String[] args) {
+        // 1. Ligue o primeiro interruptor e deixe-o ligado por alguns minutos.
+        // 2. Desligue o primeiro interruptor e ligue o segundo interruptor.
+        // 3. Vá para a sala das lâmpadas.
+
+        // - A lâmpada que estiver acesa corresponde ao segundo interruptor.
+        // - A lâmpada que estiver apagada mas quente corresponde ao primeiro interruptor.
+        // - A lâmpada que estiver apagada e fria corresponde ao terceiro interruptor.
+        
+        System.out.println("Use a lógica descrita acima para determinar qual interruptor controla qual lâmpada.");
+    }
+}
