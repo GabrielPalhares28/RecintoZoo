ea3f31f034fb6bf2514245cb4b2465836d4bfeda
Gabriel Alves Palhares <113656051+GabrielPalhares28@users.noreply.github.com>
2024-09-04 18:27:00 -0300

    Add files via upload

---
+++ SequenciaLogica.java
@@ -0,0 +1,21 @@
+public class SequenciaLogica {
+    public static void main(String[] args) {
+        // a) 1, 3, 5, 7, 9
+        System.out.println("a) 1, 3, 5, 7, 9");
+
+        // b) 2, 4, 8, 16, 32, 64, 128
+        System.out.println("b) 2, 4, 8, 16, 32, 64, 128");
+
+        // c) 0, 1, 4, 9, 16, 25, 36, 49
+        System.out.println("c) 0, 1, 4, 9, 16, 25, 36, 49");
+
+        // d) 4, 16, 36, 64, 100
+        System.out.println("d) 4, 16, 36, 64, 100");
+
+        // e) 1, 1, 2, 3, 5, 8, 13
+        System.out.println("e) 1, 1, 2, 3, 5, 8, 13");
+
+        // f) 2, 10, 12, 16, 17, 18, 19, 200
+        System.out.println("f) 2, 10, 12, 16, 17, 18, 19, 200");
+    }
+}
